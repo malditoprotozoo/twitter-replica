85347e104c228dba197e0dd5402cf7b5603c107c
malditoprotozoo <virodriguez.fe@gmail.com>
2017-11-10 22:53:27 -0300

    Version 2.0: I fix everything xD

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,155 @@
-function tweetPublish() {
-    // Tomar texto ingresado
-    var textTweet = document.getElementById("estado").value;
+// Declarando variables
 
-    if (textTweet.length == 0 || textTweet == null) {
-        alert("Debes ingresar un mensaje");
-        return false;
-    } else {
-        // Limpiar el área luego de que el tweet se envía
-        document.getElementById("estado").value = "";
+var tweetButton = document.getElementById("tweet-button");
+var publishButton = document.getElementById("publish-tweet");
+var closeIcon = document.getElementById("close-button");
+var myModal = document.getElementById("myModal");
+var tweetHere = document.getElementById("tweet-here");
+var tweetsSection = document.getElementById("tweets");
+var spanCounter = document.getElementById("counter");
 
-        var tweets = document.getElementById("tweets");
-        var containerTweets = document.createElement("article");
-        var paragraph = document.createElement("p");
-        textTweet = document.createTextNode(textTweet);
-        containerTweets.appendChild(textTweet);
-        tweets.appendChild(containerTweets);
+// var tweetHereContent = document.getElementById("tweet-here").value;
+
+tweetButton.addEventListener("click", function() {
+        myModal.style.display = "block";
+});
+
+
+closeIcon.addEventListener("click", function() {
+        myModal.style.display = "none";
+});
+
+// Para que el modal se cierre cuando haga click fuera del mismo
+window.onclick = function(event) {
+    if (event.target == myModal) {
+        myModal.style.display = "none";
     }
-
 };
+
+// Función que cuenta los caracteres, cambia los colores y deshabilita el botón
 
 function countChars(textbox, counter, max) {
-    var count = max - document.getElementById(textbox).value.length;
-    if (count < 0) {
-        document.getElementById(counter).innerHTML = "<span style=\"color: red;\">" + count + "</span>";
+    var count = max - tweetHere.value.length;
+    // El texto del contador debe ser igual al número de caracteres menos 140
+    spanCounter.textContent = count;
+    // Si el contador es igual o menor a cero
+    if (count <= 0) {
+        spanCounter.style.color = "red";
+    };
+    // Si el contador es mayor a 0, pero menor o igual a 10
+    if (count > 0 && count <= 10) {
+        spanCounter.style.color = "orange";
+    };
+    // Si el contador es mayor a 10, pero menor o igual a 20
+    if (count > 10 && count <= 20) {
+        spanCounter.style.color = "yellow";
+    };
+    // Si el contador es mayor a 20
+    if (count > 20) {
+        spanCounter.style.color = "black";
+    };
+
+    // Si el contador es distinto de 140 y mayor a cero, el botón está habilitado
+    if (count !== 140 && count >= 0) {
+        publishButton.disabled = false;
+    // En caso contrario, está deshabilitado
     } else {
-        document.getElementById(counter).innerHTML = count;
+        publishButton.disabled = true;
+    };
+
+    if (tweetHere.scrollTop != 0) {
+        tweetHere.style.height = tweetHere.scrollHeight + "px";
     }
 };
+
+function tweetPublish() {
+    var textTweet = tweetHere.value;
+
+    if (textTweet.length !== 0) {
+        var containerTweet = document.createElement("div");
+        var textTag = document.createElement("span");
+        textTweet = document.createTextNode(textTweet);
+        textTag.appendChild(textTweet);
+        containerTweet.appendChild(textTag);
+        tweetsSection.insertBefore(containerTweet, tweetsSection.children[0]);
+
+        textTag.classList.add("tweetItself");
+        containerTweet.classList.add("tweeted");
+
+        // Añadiendo el nombre
+        var name = document.createTextNode("Tori Rodríguez");
+        var containerName = document.createElement("span");
+        containerName.appendChild(name);
+        containerTweet.insertBefore(containerName, textTag);
+
+        containerName.classList.add("name");
+
+        // Añadiendo el nickname
+        var nickname = document.createTextNode("@virodriguezfe");
+        var containerNickname = document.createElement("span");
+        containerNickname.appendChild(nickname);
+        containerTweet.insertBefore(containerNickname, textTag);
+
+        containerNickname.classList.add("nickname");
+
+        // Añadiendo la foto de perfil
+
+        var profilePicture = document.createElement("img");
+        profilePicture.setAttribute("src", "assets/images/profile-popcorn.png");
+        profilePicture.classList.add("profile-pic-in-tweet");
+        containerTweet.insertBefore(profilePicture, containerName);
+
+        // Determinando y añadiendo la fecha
+        var date = new Date();
+        var day = date.getDate().toString();
+        var month = date.getMonth().toString();
+        var year = date.getFullYear().toString();
+        
+        var hour = date.getHours();
+
+        if (hour <= 9) {
+            hour = "0" + hour.toString();
+        } else {
+            hour = hour.toString();
+        };
+
+        var minutes = date.getMinutes();
+
+        if (minutes <= 9) {
+            minutes = "0" + minutes.toString();
+        } else {
+            minutes = minutes.toString();
+        };
+
+        var seconds = date.getSeconds();
+
+        if (seconds <= 9) {
+            seconds = "0" + seconds.toString();
+        } else {
+            seconds = seconds.toString();
+        };
+
+        var textTime = document.createTextNode(day + "/" + month + "/" + year + " " + hour + ":" + minutes + ":" + seconds);
+        var containerTime = document.createElement("span");
+        containerTime.appendChild(textTime);
+        containerTime.classList.add("date");
+
+        containerTweet.insertBefore(containerTime, textTag);
+
+        var containerNickname = document.createElement("span");
+        // Limpiar el área luego de que el tweet se envía
+        document.getElementById("tweet-here").value = "";
+        myModal.style.display = "none";
+    };
+};
+
+// Función que desactiva el botón si el área de texto está vacía
+
+// tweetHere.addEventListener("focus", function() {
+//     if (tweetHere.value.length === 0) {
+//         publishButton.setAttribute("disabled", "disabled");
+//     } else {
+
+//     }
+// });
+
